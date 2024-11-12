8f84a1500ee867550ff05b62486b29c685117dcc
David Lilley <davidlilley444@outlook.com>
2024-11-12 09:24:05 +0000

    added signup

--- server.py
+++ server.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template ("index.html")
 
+@app.route("/signup",methods=['GET','POST'])
+def signup():
+    return render_template('signup.html')
+
 
