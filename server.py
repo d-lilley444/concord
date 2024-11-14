4409b5dc5dc4c727180f268a97c2c31f6ffad5b1
David Lilley <davidlilley444@outlook.com>
2024-11-14 15:22:18 +0000

    added routes for signup, login, logout and templates

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for,g
+from flask import Flask, render_template,url_for,g,redirect,request
 import sqlite3
 import json
 import hashlib
@@ -84,7 +84,86 @@ def request_loader(request):
         return None
 
 
+@app.route('/signup',methods=['GET','POST'])
+def signup():
 
+    if request.method == 'GET':
+        return render_template("signup.html")
+
+    if request.method == 'POST':
+
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if email == None or username == None or password == None:
+            return render_template("signup.html", errors = "somethings missing")
+       
+        #hash the password
+        hashed = hashlib.sha256(password.encode())
+
+        con = get_db()
+        cur = con.cursor()
+
+        sql = "INSERT INTO users (username,email,hash) VALUES (?,?,?)"
+        valaues = (username,email,hashed.hexdigest())
+        cur.execute(sql,valaues)
+        con.commit()
+        #probably need other checks here ??
+
+        return redirect(url_for('login'))
+
+    #chect the posted info
+
+
+@app.route("/login",methods = ['GET','POST'])
+def login():
+
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if email == None or password == None:
+            return render_template("login.html", errors = "somethings missing")
+       
+        #hash the password
+        hashed = hashlib.sha256(password.encode())
+
+        con = get_db()
+        cur = con.cursor()
+
+        sql = "SELECT * FROM users WHERE email = ?"
+        valaues = (email,)
+        cur.execute(sql,valaues)
+        user_info = cur.fetchone()
+
+        app.logger.info(f"USER LGIN INFO = {user_info[0]}, {user_info[1]}, {user_info[2]}")
+
+        if user_info == None:
+            return render_template("login.html",errors="Soemthing went wrong")
+
+        if hashed.hexdigest() == user_info[3]:
+            user = User()
+            user.id = user_info['user_id']
+            user.username = user_info['username']
+            user.email = user_info['email']
+
+            flask_login.login_user(user)
+            return redirect(url_for('profile'))
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    flask_login.logout_user()
+    return redirect(url_for('login'))
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    #return "Unauthorized", 401
+    return redirect(url_for('login'))
 
 
 
@@ -92,9 +171,4 @@ def request_loader(request):
 @app.route("/")
 def index():
     return render_template ("index.html")
-
-@app.route("/signup",methods=['GET','POST'])
-def signup():
-    return render_template('signup.html')
-
 
